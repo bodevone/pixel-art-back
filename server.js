7d31d2319d505405eeef93d7c4f97bfd9ae756c7
Alibi Shalgymbay <aliby.bbb@gmail.com>
2020-02-29 21:16:47 +0600

    Database created

--- server.js
+++ server.js
@@ -10,31 +10,14 @@ mongo.connect(process.env.MONGODB_URI || 'mongodb://localhost/mongopixel', {useU
 
   console.log('MongoDB connected...')
 
-  let db = database.db(process.env.MONGODB_NAME || 'mongopixel')
-  let pixelsCollection = db.collection('pixels')
-
-  var pixels = []
-  var id = 1
-  for (var i=0; i<20; i++) {
-    for (var j=0; j<20; j++) {
-      pixels.push({"id": id, "row": i, "column": j, "color": "FFFFFF"})
-      id++
-    }
-  }
-
-  pixelsCollection.insert(pixels)
-
-
-
-
 
   // server.listen(4000, () => {
   console.log('Listening on port 4000...')
 
   // Connect to socket io
   io.on('connection', (socket) => {
-    db = database.db(process.env.MONGODB_NAME || 'mongopixel')
-    pixelsCollection = db.collection('pixels')
+    let db = database.db(process.env.MONGODB_NAME || 'mongopixel')
+    let pixelsCollection = db.collection('pixels')
 
     console.log("New connection...")
 
